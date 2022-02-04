a32daf4f6f1d465cc4ced6d5a2b959a7cda2a0f5
Areccus Branch <areccusbranch@AreccussMacBook.wirelessinternet>
2022-02-04 05:59:44 -0500

    just keep swimming

--- api/server.js
+++ api/server.js
@@ -3,17 +3,17 @@ const cors = require('cors');
 const helmet = require('helmet');
 const authRouter = require('./auth/auth-router')
 const plantsRouter = require('./plants/plants-router')
-const restricted = require('./restricted')
 const server = express();
 
 server.use(helmet());
-server.use(cors());
 server.use(express.json());
+server.use(cors());
 
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
+
 server.use('/api/auth', authRouter)
 server.use('/api/plants', plantsRouter)
 
